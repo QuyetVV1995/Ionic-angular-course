918673aedce42659cf14b86da32ae570031ad471
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-22 14:52:50 +0700

    understan event binding

--- ng-refresher/src/app/persons/person-input.component.ts
+++ ng-refresher/src/app/persons/person-input.component.ts
@@ -6,4 +6,8 @@ import { Component } from "@angular/core";
   templateUrl: './person-input.component.html',
   styleUrls: ['./person-input.component.css']
 })
-export class PersonInputComponent{}
+export class PersonInputComponent{
+  onCreatePerson(){
+    console.log('Create a person');
+  }
+}
