8b58b98c801096f62c5cf8079d8a343c61680072
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-22 14:49:19 +0700

    Handing user input

--- ng-refresher/src/app/app.module.ts
+++ ng-refresher/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { from } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { PersonsComponent } from "./persons/persons.component";
+import { PersonInputComponent } from "./persons/person-input.component"
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonsComponent
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
     BrowserModule
